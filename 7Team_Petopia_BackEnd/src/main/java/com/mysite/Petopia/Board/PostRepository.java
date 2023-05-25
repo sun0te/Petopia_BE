861f09cfc9d0a5376379ef3b6440d79952389859
Choiheeju59 <85815718+Choiheeju59@users.noreply.github.com>
2023-05-25 09:33:48 +0900

    Delete PostRepository.java

--- 7Team_Petopia_BackEnd/src/main/java/com/mysite/Petopia/Board/PostRepository.java
+++
@@ -1,6 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
