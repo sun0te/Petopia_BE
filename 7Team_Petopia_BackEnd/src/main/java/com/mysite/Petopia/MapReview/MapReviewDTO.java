b8a5a73564583cc196a5740e4fb5a6c334424329
dlrdyd97 <dlrdyd97@naver.com>
2023-05-26 23:03:56 +0900

    .

--- 7Team_Petopia_BackEnd/src/main/java/com/mysite/Petopia/MapReview/MapReviewDTO.java
+++ 7Team_Petopia_BackEnd/src/main/java/com/mysite/Petopia/MapReview/MapReviewDTO.java
@@ -1,10 +1,12 @@
 package com.mysite.Petopia.MapReview;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.mysite.Petopia.Map.MapDTO;
 import com.mysite.Petopia.Users.UsersDTO;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,6 +17,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import lombok.Getter;
--- 7Team_Petopia_BackEnd/src/main/java/com/mysite/Petopia/MapReview/ReviewImgRepository.java
+++ 7Team_Petopia_BackEnd/src/main/java/com/mysite/Petopia/MapReview/ReviewImgRepository.java
@@ -3,5 +3,7 @@ package com.mysite.Petopia.MapReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewImgRepository extends JpaRepository<ReviewImgDTO, Long> {
+	
+	void deleteAllByReview(MapReviewDTO review);
 
 }
